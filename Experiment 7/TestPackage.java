79c4af645fc9afa5e192a4252b63f0a8c0afe38d
Vanshika609 <24csu310@ncuindia.edu>
2025-10-16 14:52:16 +0530

    Add files via upload

---
+++ Experiment 7/TestPackage.java
@@ -0,0 +1,8 @@
+import exercises.java.PackageDemo;
+
+public class TestPackage {
+    public static void main(String[] args) {
+        PackageDemo obj = new PackageDemo();
+        obj.display();
+    }
+}
